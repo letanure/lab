2c6c79c8213be67ec65e9cf04468efedff4a8c9b
Luiz Tanure <letanure@gmail.com>
2021-12-13 15:37:50 +0100

    refactor: move demo to file

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,7 @@
-import { templateEngine } from "./templateEngine";
+import runTemplateenfine from "./demoTemplateEngine";
 
-import "./style.css";
+// const runTests = [
+//   ...new URLSearchParams(new URL(location.href).search).entries(),
+// ].reduce((acc, item) => ({ ...acc, [item[0]]: item[1] }), {});
 
-const template =
-  "Hello, my name is {{name}}. Im {{age}} years old. I am based in {{based}}.";
-const result = templateEngine(template, {
-  name: "Luiz",
-  age: 40,
-  based: "Berlin",
-});
-
-const app = document.querySelector<HTMLDivElement>("#app")!;
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-  <p>${result}</p>
-`;
+runTemplateenfine();
